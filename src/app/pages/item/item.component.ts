ddeb8d3c59848af7b042c304955912962c5030d1
Jamel Yassin <jamelyassin84@gmail.com>
2021-11-07 19:37:49 +0400

    item

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import {
+	ActivatedRoute,
+	ActivatedRouteSnapshot,
+	ParamMap,
+} from '@angular/router'
+import { EndPoints } from 'src/app/services/Api'
+import { BaseService } from 'src/app/services/base.service'
 
 @Component({
-  selector: 'app-item',
-  templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+	selector: 'app-item',
+	templateUrl: './item.component.html',
+	styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
+	constructor(private service: BaseService, private route: ActivatedRoute) {
+		this.route.paramMap.subscribe((params: ParamMap) => {
+			this.product = params.get('id')
+		})
+	}
 
-  constructor() { }
+	product!: any
 
-  ngOnInit(): void {
-  }
+	productInfo: any = {}
 
+	ngOnInit(): void {
+		this.getProduct()
+	}
+
+	getProduct() {
+		new BaseService(this.service.http, EndPoints.products, '')
+			.show(this.product)
+			.subscribe((data) => {
+				this.productInfo = data.product
+				console.log(data.product)
+			})
+	}
 }
